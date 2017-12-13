9966a81c3baeed10188ac99de4b41e07741845f9
BEE <bee@rakuya.com.tw>
2017-12-13 17:34:51 +0800

    scroll animate done

--- js/main.js
+++ js/main.js
@@ -10,18 +10,30 @@ $(function() {
   $(".soundcloud").toggleClass("active");
   });
 
-  // //Scroll Controll
-  // $(window).scroll( function(){
-  //       /* Check the location of each desired element */
-  //       $('.hideme').each( function(i){
-  //           var bottom_of_object = $(this).offset().top + $(this).outerHeight();
-  //           var bottom_of_window = $(window).scrollTop() + $(window).height();
-  //           /* If the object is completely visible in the window, fade it it */
-  //           if( bottom_of_window > bottom_of_object ){
-  //               $(this).animate({'opacity':'1'},500);
-  //           }
-  //       });
-  //   });
+  //Scroll Controll
+	$('#about, .action').click(function(){
+		$('html, body').animate({scrollTop: $('.about').offset().top }, 'slow');
+		return false;
+	});
+
+  $('#work').click(function(){
+    $('html, body').animate({scrollTop: $('.works').offset().top }, 'slow');
+    return false;
+  });
+
+  $('#mabout').click(function(){
+		$('html, body').animate({scrollTop: $('.about').offset().top }, 'slow');
+    $(".menu-overlay").toggleClass("active");
+    $(".m-menu").toggleClass("active");
+    return false;
+	});
+
+  $('#mwork').click(function(){
+    $('html, body').animate({scrollTop: $('.works').offset().top }, 'slow');
+    $(".menu-overlay").toggleClass("active");
+    $(".m-menu").toggleClass("active");
+    return false;
+  });
 
 
 });
